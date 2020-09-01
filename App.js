ea0ef34759fa0aee3ff2d25fffe52ea1f5507bcc
Maya <melokitty.1003@gmail.com>
2020-09-01 19:30:45 +0900

    first commit

--- App.js
+++ App.js
@@ -1,12 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button} from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{flex : 1, aligItem: 'center', justifyContent: 'center'}}>
+      <Button title="Pick Image from Folder" onPress={() => alert("hello")} />
     </View>
   );
 }
